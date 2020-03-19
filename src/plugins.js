861e65ba37c4089663ef5561706c032882cba10e
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2020-03-19 13:58:25 +0100

    WIP: make it possible to use react hooks in plugin

--- src/plugins.js
+++ src/plugins.js
@@ -1,9 +1,16 @@
-// TODO: import plugins dynamically with import()
-import { extensions as lisaPluginExtensions } from 'lisa-plugin'
-import { locales as lisaPluginLocales } from 'lisa-plugin'
+import { merge } from 'lodash'
 
-const pluginsExtensions = [...lisaPluginExtensions]
-const pluginsLocales = lisaPluginLocales
+const pluginsExtensions = []
+const pluginsLocales = {}
+const importAll = (r) => { return r.keys().map(r) }
+const modules = importAll(require.context('./plugins', false, /\.js$/))
+
+function registerPlugin({ default: { extensions, locales }}) {
+  extensions.map(ext => pluginsExtensions.push(ext))
+  merge(pluginsLocales, locales)
+}
+
+modules.map(registerPlugin)
 
 export {
   pluginsExtensions,
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+module.exports = {
+  resolve: {
+    alias: {
+      react: path.resolve('./node_modules/react')
+    }
+  }
+}
