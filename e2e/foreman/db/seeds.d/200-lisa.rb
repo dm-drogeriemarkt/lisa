2fa29a6a007e4bbad7f0818c0dddd83af919af4c
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2021-06-29 12:28:39 +0200

    e2e tests

---
+++ e2e/foreman/db/seeds.d/200-lisa.rb
@@ -0,0 +1,50 @@
+all_locations = Location.all
+all_organizations = Organization.all
+
+smart_proxy = SmartProxy.create!(
+  name: 'SmartProxyLisa',
+  url: 'http://localhost:8800',
+  locations: all_locations,
+  organizations: all_organizations
+).tap do |smart_proxy|
+  PuppetClassImporter.new({ url: smart_proxy.url }).changes['new'].map do |key, value|
+    PuppetClassImporter.new.send(:add_classes_to_foreman, key, value)
+  end
+end
+
+domain = Domain.create!(
+  name: 'development.example.com',
+  locations: all_locations,
+  organizations: all_organizations
+).tap do |domain|
+  domain.subnets.create!(
+    name: 'SubnetLisa',
+    network: '127.0.0.1',
+    mask: '255.255.255.0',
+    vlanid: 1,
+    locations: all_locations,
+    organizations: all_organizations
+  )
+end
+
+ComputeResource.create!(
+  name: 'ComputeResourceLisa ',
+  provider: 'Vmware',
+  uuid: 'Solutions',
+  url: 'http://localhost:8283',
+  user: 'user',
+  password: 'password',
+  http_proxy_id: smart_proxy.id,
+  domain: domain,
+  caching_enabled: false,
+  locations: all_locations,
+  organizations: all_organizations
+)
+
+Operatingsystem.create!(
+  name: 'OSLisa',
+  major: 1,
+  media: Medium.all,
+  ptables: Ptable.all,
+  architectures: Architecture.all
+)
