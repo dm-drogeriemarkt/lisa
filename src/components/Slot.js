d64e206d03762fc0c109c222f14711b83ba5b44b
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2022-05-26 12:33:31 +0200

    Allow to pass down props from core to plugins

--- src/components/Slot.js
+++ src/components/Slot.js
@@ -2,15 +2,15 @@ import React, { Fragment } from 'react'
 import { pluginsExtensions } from 'plugins'
 import pipe from 'helpers/pipe'
 
-const Slot = ({ name, elements }) => {
+const Slot = ({ name, elements, ...sharedProps }) => {
   const extensions = pluginsExtensions.filter(({ slot }) => slot === name).map(({ extension }) => extension)
   const withExtensions = extensions.length ? pipe(...extensions)(elements) : elements
 
   return (
     <Fragment>
       { withExtensions.map(({
-        components: [InitialComponent, ...components], ...attrs
-      }, i) => <InitialComponent key={i} components={components} {...attrs} />) }
+        components: [InitialComponent, ...components], ...props
+      }, i) => <InitialComponent key={i} components={components} {...sharedProps} {...props} />) }
     </Fragment>
   )
 }
