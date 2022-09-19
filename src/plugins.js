bdfecd2ba76763f6b6f9a118438a12d8f7dcc002
Kamil Szubrycht <kamil.szubrycht@ironin.pl>
2022-09-19 16:17:26 +0200

    Fix regex to avoid duplicate plugins

--- src/plugins.js
+++ src/plugins.js
@@ -5,7 +5,7 @@ const areWeTestingWithJest = process.env.JEST_WORKER_ID !== undefined
 const pluginsExtensions = []
 const pluginsLocales = {}
 const importAll = (r) => { return r.keys().map(r) }
-const modules = areWeTestingWithJest ? [] : importAll(require.context('./plugins', false, /\.js$/))
+const modules = areWeTestingWithJest ? [] : importAll(require.context('./plugins', false, /plugins\/.*\.js$/))
 
 function registerPlugin({ default: { extensions, locales }}) {
   extensions.map(ext => pluginsExtensions.push(ext))
